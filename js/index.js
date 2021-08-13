5ea6149eba4ce4f1c1959a41b3e39d710dd44276
saphirevert <jerome.cosandey@gmail.com>
2021-08-13 17:59:14 +0200

    [chore] Non au code mort ! (index.js)

--- js/index.js
+++ js/index.js
@@ -10,15 +10,8 @@ const tableInit = new Table(Utils, Tests)
 import { eventHandlers } from './EventHandlers.js';
 eventHandlers(Table, Utils)
 
-
-// const tototo = new Table();
-
-
 let testTableBtn = document.getElementById('testBtn');
 testTableBtn.addEventListener('click', function() {
-  // console.log(Utils.test);
-  // console.log(table.csvText);
-  // console.log(table.convertedText);
   console.log(testsInit.test);
   console.log(Tests.test);
 
