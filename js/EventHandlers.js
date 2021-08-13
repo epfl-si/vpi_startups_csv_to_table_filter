033d78efef51b37915f2f060dd477585ee873ff0
saphirevert <jerome.cosandey@gmail.com>
2021-08-13 16:32:15 +0200

    [Chore] Moved `buildTable` function from `EventHandlers.js` to `Table.js`

--- js/EventHandlers.js
+++ js/EventHandlers.js
@@ -52,59 +52,10 @@ export function eventHandlers (table, Utils) {
 
   async function uploadFile(file) {
     table.csvText = await file.text()
-    await buildTable(table.csvText)
+    await table.buildTable(table.csvText)
   }
 
-  // Cherrypicked from https://github.com/yasharma/CsvToTable
-  function buildTable(csvFileString) {
-    var activeColumns = [0,1,2,15,17];
-    var prefix = `<!-- wp:epfl/table-filter {"largeDisplay":true,"tableHeaderOptions":"header,sort"} -->\n<!-- wp:table {"className":"is-style-stripes"} -->\n<figure class="wp-block-table is-style-stripes">\n`;
-    var suffix = `</figure>\n<!-- /wp:table -->\n<!-- /wp:epfl/table-filter -->`
-    var allRows = csvFileString.split(/\r?\n|\r/).filter(isNotEmpty);
-    var strTable = prefix;
-    strTable += '<Table>';
-    for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
-      if (singleRow === 0) {
-        strTable += '<thead>';
-        strTable += '<tr>';
-      } else {
-        strTable += '<tr>';
-      }
-      var rowCells = allRows[singleRow].split(',');
-      for(var rowCell = 0; rowCell < rowCells.length; rowCell++){
-        if(activeColumns.includes(rowCell)){
-          if(singleRow === 0){
-            strTable += '<th>';
-            strTable += rowCells[rowCell];
-            strTable += '</th>';
-          } else {
-            strTable += '<td>';
-            strTable += rowCells[rowCell];
-            strTable += '</td>';
-          }
-        }
-      }
-      if (singleRow === 0) {
-        strTable += '</tr>';
-        strTable += '</thead>';
-        strTable += '<tbody>';
-      } else {
-        strTable += '</tr>';
-      }
-    }
-    strTable += '</tbody>';
-    strTable += '</Table>';
-    strTable += suffix;
-    table.convertedText = strTable
-    document.getElementById('preview').innerHTML = strTable;
-    document.getElementById('rawCode').textContent = strTable;
-    Utils.download("wp-strTable.txt", strTable)
-    return strTable
 
-    function isNotEmpty(row) {
-      return row !== "";
-    }
-  }
 
 
 
--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-import { Table } from './Table.js';
-const table = new Table();
 
 import { Utils } from './Utils.js';
 const utils = new Utils();
 
+import { Table } from './Table.js';
+const table = new Table(Utils);
 
 import { eventHandlers } from './EventHandlers.js';
 eventHandlers(table, Utils)
