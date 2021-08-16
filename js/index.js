eb5e2ca6cae65f852824115d433800fc922c8502
saphirevert <jerome.cosandey@gmail.com>
2021-08-16 15:37:58 +0200

    [chore] add lib folder

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 
 //Importing all modules
-import { Modules } from './Modules.js';
+import { Modules } from './lib/Modules.js';
 
-import { Utils } from './Utils.js';
+import { Utils } from './lib/Utils.js';
 const utilsInit = new Tests()
 Modules.Utils = Utils
 
-import { Tests } from './Tests.js';
+import { Tests } from './lib/Tests.js';
 const testsInit = new Tests()
 Modules.Tests = Tests
 
-import { Table } from './Table.js';
+import { Table } from './lib/Table.js';
 const tableInit = new Table(Modules)
 Modules.Table = Table
 
 
 
 // Launching app
-import { EventHandlers } from './EventHandlers.js';
+import { EventHandlers } from './lib/EventHandlers.js';
 const eventHandlersInit = new EventHandlers(Modules)
 
 EventHandlers.load()
